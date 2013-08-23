92100252264324d246f273b09604e468fa852225
Justin French <justin@indent.com.au>
2013-08-24 09:38:25 +1000

    initial commit Rails 4.0.0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TreedocExample::Application.config.secret_key_base = 'b2800ea1fd902d86bcebe789e1925dd0e6a6e483e21918ea0d8c80c579f40f404a1e922b779119e4652c2814f01453db3510041b5de7288ce7251c53ce1fe7d4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TreedocExample::Application.config.session_store :cookie_store, key: '_treedoc-example_session'
