7d4c41f0a7fdb33e2f8c16813d33caab631dda79
Justin French <justin@indent.com.au>
2013-08-24 09:47:23 +1000

    add faqs route to treedoc

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 TreedocExample::Application.routes.draw do
+  
+  get '/:treedoc(/*path)' => 'treedoc/documents#show', constraints: { :treedoc => /faqs/ }
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
